615a5752b6cc51006ea03b2865dcfa87a1f0ecf2
Elliott Record <elliott.record@288group.com>
2021-10-18 09:19:58 +0100

    Added BottomNavigationButtons

--- 03-basic-widgets/projects/starter/lib/home.dart
+++ 03-basic-widgets/projects/starter/lib/home.dart
@@ -26,7 +26,17 @@ class _HomeState extends State<Home> {
           child: Text('Let\'s get cooking 👩‍🍳',
               // 3
               style: Theme.of(context).textTheme.headline1)),
-      // TODO: Add bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        items: [
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card'),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card2'),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.card_giftcard), label: 'Card3'),
+        ],
+      ),
     );
   }
 }
