2ce7321ab10fcefe00e15ec2b833c3f4eaacb82c
Elliott Record <elliott.record@288group.com>
2021-11-01 23:04:13 +0000

    Added inputs

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
@@ -3,6 +3,8 @@ import 'package:fooderlich/models/models.dart';
 import 'package:fooderlich/screens/empty_grocery_screen.dart';
 import 'package:provider/provider.dart';
 
+import 'grocery_item_screen.dart';
+
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
@@ -11,7 +13,28 @@ class GroceryScreen extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
           onPressed: () {
-            // TODO 11 Present GroceryItemsScreen
+            // 1
+            final groceryManager =
+                Provider.of<GroceryManager>(context, listen: false);
+// 2
+            Navigator.push(
+              context,
+              // 3
+              MaterialPageRoute(
+                // 4
+                builder: (context) => GroceryItemScreen(
+                  // 5
+                  onCreate: (item) {
+                    // 6
+                    groceryManager.addItem(item);
+                    // 7
+                    Navigator.pop(context);
+                  },
+                  // 8
+                  onUpdate: (item) {},
+                ),
+              ),
+            );
           },
         ),
         body: buildGroceryScreen(),
