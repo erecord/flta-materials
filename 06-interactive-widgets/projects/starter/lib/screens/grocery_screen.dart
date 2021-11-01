3575ca70d73c8cbc5158690c447c9aff6cad5245
Elliott Record <elliott.record@288group.com>
2021-11-01 22:31:01 +0000

    Added Scaffold with floating Add button

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/models/models.dart';
 import 'package:fooderlich/screens/empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const EmptyGroceryScreen();
+  Widget build(BuildContext context) => Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            // TODO 11 Present GroceryItemsScreen
+          },
+        ),
+        body: buildGroceryScreen(),
+      );
+
+  Widget buildGroceryScreen() {
+    // 1
+    return Consumer<GroceryManager>(
+      // 2
+      builder: (context, groceryManager, child) {
+        // 3
+        if (groceryManager.groceryItems.isNotEmpty) {
+          // TODO 25: Add GroceryListScreen
+          return Container();
+        } else {
+          // 4
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
   }
-  // TODO: Add buildGroceryScreen
 }
